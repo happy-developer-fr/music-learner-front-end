28465e7d71d8773dbb9597583bb86d14a3d076b7
cottet julien <julien.cottet@group.leguide.com>
2020-02-18 22:25:24 +0100

    Add Menu

---
+++ src/component/MainNavComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Component } from "react";
+
+class MainNav extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+          <div class="container">
+            <a class="navbar-brand" href="#">Music Learner</a>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default MainNav;
